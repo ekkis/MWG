1c1a7e3529918ef3868356eb44019304507128c1
greatdolphinls <great.dolphin.ls@gmail.com>
2020-06-02 12:45:55 +0300

    [feat] implement font

---
+++ styles/global.js
@@ -0,0 +1,18 @@
+
+import css from 'styled-jsx/css';
+
+export default css.global`
+  @font-face {
+    font-family: 'Graphik';
+    src: url('/fonts/Graphik-Black.otf');
+    src: url('/fonts/Graphik-Bold.otf');
+    src: url('/fonts/Graphik-Medium.otf');
+    src: url('/fonts/Graphik-Regular.otf');
+    src: url('/fonts/Graphik-Semibold.otf');
+    src: url('/fonts/Graphik-Thin.otf');
+  }
+  @font-face {
+    font-family: 'Dirtyego';
+    src: url('/fonts/DIRTYEGO.TTF');
+  }
+`;
